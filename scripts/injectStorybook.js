cfa1a754d626a33776718e533f2626bfacfd1c21
Mike <ssbeefeater@gmail.com>
2018-04-12 09:53:23 +0300

    add header in react-types

---
+++ scripts/injectStorybook.js
@@ -0,0 +1,12 @@
+
+const topBar = `<div style="width:100%">
+
+<h3>You may also like</h3>
+<div style="width:100%;display:flex;margin-top:-8px;">
+    <a style="margin-right:8px;color:#212121!important;" href="https://github.com/ssbeefeater/react-localizer">react-localizer</a>
+    <a style="margin-right:8px;color:#212121!important;"  href="https://github.com/ssbeefeater/react-smart-form">react-smart-form</a>
+</div>
+</div>`
+
+
+document.querySelector(".Pane.horizontal").innerHTML = topBar+document.querySelector(".Pane.horizontal").innerHTML
